2ef447eb4feb727eae09fc11ecbec2a71a4b6d28
mae-x99 <farhanazainal.a99@gmail.com>
2026-04-10 06:40:53 +0800

    Fix: Added base in vite.config.ts & remove './public' from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -675,7 +675,7 @@ function App() {
           <section className="relative h-[85vh] flex items-center overflow-hidden">
         <div className="absolute inset-0 z-0">
           <img 
-            src="./public/Mount-Kinabalu.jpg" 
+            src="/Mount-Kinabalu.jpg" 
             alt="Borneo Nature" 
             className="w-full h-full object-cover brightness-75"
             referrerPolicy="no-referrer"
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss(), viteSingleFile()],
+    base: '/1-stop-borneo-mockup/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
